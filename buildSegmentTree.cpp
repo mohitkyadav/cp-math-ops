c5600e97803be64c735978e08d6f376e841c670d
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2018-08-19 17:04:58 +0530

    remove exe

--- buildSegmentTree.cpp
+++ buildSegmentTree.cpp
@@ -17,7 +17,7 @@ void buildTree(int* arr, int* tree, int start, int end, int treeNode) {
 
 int main() {
     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int* tree = new int[18];
+    int* tree = new int[1000];
     buildTree(arr, tree, 0, 8, 1);
     for(size_t i = 1; i < 18; i++) {
         cout << tree[i] << endl;
